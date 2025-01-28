109db961368c28679f810ea8b3e83700c1ba898f
Dominik Ivanov <ivanov.dominik@student.tuke.sk>
2025-01-28 20:34:21 +0100

    move done

--- Game.c
+++ Game.c
@@ -3,8 +3,7 @@
 #include <time.h>
 
 void print_grid(int **grid, int size) {
-    //system("clear || cls");
-    system("cls >nul 2>&1 || clear >nul 2>&1");
+    system("clear || cls");
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size*4+1; j++) {
             printf("-");
@@ -41,6 +40,9 @@ void add_number(int **grid, int size, int score) {
         j = rand() % size;
     }while (grid[i][j] != 0);
     grid[i][j] = (value == 1) ? 4 : 2;
+    if(score==-1){
+        add_number(grid,size,score+1);
+    }
 }
 
 int movement(int **grid, int size, int dir) {
@@ -60,6 +62,7 @@ int movement(int **grid, int size, int dir) {
                             score += grid[i][j] * 2;
                             grid[i][jj - 1] *= 2;
                             grid[i][j] = 0;
+                            break;
                         }
 
                     }
@@ -73,19 +76,129 @@ int movement(int **grid, int size, int dir) {
         for (int i = 0; i < size; i++) {
             for (int j = 1; j < size; j++) {
                 if (grid[i][j] != 0) {
-
+                    for (jj = 0; jj < j; jj++) {
+                        if (grid[i][jj] == 0) {
+                            grid[i][jj] = grid[i][j];
+                            grid[i][j] = 0;
+                            break;
+                        }
+                    }
                 }
             }
         }
     }
 
+    //MOVE RIGHT
+    if (dir == 2) {
+        //NUMBERS COLLIDE
+        for (int i = 0; i < size; i++) {
+
+            for (int j = size-2; j >= 0; j--) {
+
+                if (grid[i][j] != 0) {
+                    //jj=j;
+                    for (jj = j; jj < size; jj++) {
+
+                        if (grid[i][jj +1] == grid[i][j]) {
+                            score += grid[i][j] * 2;
+                            grid[i][jj + 1] *= 2;
+                            grid[i][j] = 0;
+                            break;
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+        //MOVEMENT
+        for (int i = 0; i < size; i++) {
+            for (int j = 1; j < size; j++) {
+                if (grid[i][j] != 0) {
+                    for (jj = size-1; jj > 0; jj--) {
+                        if (grid[i][jj] == 0) {
+                            grid[i][jj] = grid[i][j];
+                            grid[i][j] = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //MOVE UP
+    if(dir == 1){
+        //NUMBERS COLLIDE
+        for (int j = 0; j < size; ++j) {
+            for(int i = 1; i<size;i++){
+                if(grid[i][j]!=0){
+                    for(ii=i;ii>=0;ii--){
+                        if(grid[ii][j]==grid[i][j]){
+                            score += grid[i][j] * 2;
+                            grid[ii][j] *= 2;
+                            grid[i][j] = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        //MOVEMENT
+        for (int j = 0; j < size; ++j) {
+            for(int i = 1; i<size;i++){
+                if(grid[i][j]!=0){
+                    for(ii=i;ii>=0;ii--){
+                        if(grid[ii][j]==0){
+                            grid[ii][j] = grid[i][j];
+                            grid[i][j]=0;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if(dir == 3){
+        //NUMBERS COLLIDE
+        for (int j = 0; j < size; ++j) {
+            for(int i = size-2; i>=0;i--){
+                if(grid[i][j]!=0){
+                    for(ii=i;ii<size;ii++){
+                        if(grid[ii][j]==grid[i][j]){
+                            score += grid[i][j] * 2;
+                            grid[ii][j] *= 2;
+                            grid[i][j] = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        //MOVEMENT
+        for (int j = 0; j < size; ++j) {
+            for(int i = size-2; i>=0;i--){
+                if(grid[i][j]!=0){
+                    for(ii=i;ii<size;ii++){
+                        if(grid[ii][j]==0){
+                            grid[ii][j] = grid[i][j];
+                            grid[i][j]=0;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
     return score;
 }
 
 int main(){
     srand(time(NULL));
     printf("Hello, choose size of the game(4-9): ");
-    int size,score=0;
+    int size,score=-1;
     do{
         scanf("%d",&size);
     }while(size<4||size>9);
@@ -110,9 +223,12 @@ int main(){
             board[i][j]=0;
         }
     }
-    system("cls");
-    print_grid(board,size);
     add_number(board, size, score);
+    print_grid(board,size);
+    score=0;
+    movement(board,size,0);
+
+    print_grid(board,size);
 
 
     free_grid( board, size);

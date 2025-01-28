e38d2918b5cbf1ea21a2391db1f798f88118043c
Dominik Ivanov <ivanov.dominik@student.tuke.sk>
2025-01-28 19:29:30 +0100

    start of movement,

--- Game.c
+++ Game.c
@@ -33,13 +33,62 @@ void free_grid(int **grid, int size) {
     free(grid);
 }
 
+void add_number(int **grid, int size, int score) {
+    int i,j;
+    int value = rand() % 10;
+    do{
+        i = rand() % size;
+        j = rand() % size;
+    }while (grid[i][j] != 0);
+    grid[i][j] = (value == 1) ? 4 : 2;
+}
+
+int movement(int **grid, int size, int dir) {
+    //MOVE LEFT
+    int jj, ii, score = 0;
+    if (dir == 0) {
+        //NUMBERS COLLIDE
+        for (int i = 0; i < size; i++) {
+
+            for (int j = 1; j < size; j++) {
+
+                if (grid[i][j] != 0) {
+                    //jj=j;
+                    for (jj = j; jj > 0; jj--) {
+
+                        if (grid[i][jj - 1] == grid[i][j]) {
+                            score += grid[i][j] * 2;
+                            grid[i][jj - 1] *= 2;
+                            grid[i][j] = 0;
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+        //MOVEMENT
+        for (int i = 0; i < size; i++) {
+            for (int j = 1; j < size; j++) {
+                if (grid[i][j] != 0) {
+
+                }
+            }
+        }
+    }
+
+    return score;
+}
 
 int main(){
+    srand(time(NULL));
     printf("Hello, choose size of the game(4-9): ");
-    int size=10;
-    while(size<4||size>9){
+    int size,score=0;
+    do{
         scanf("%d",&size);
-    }
+    }while(size<4||size>9);
     int **board = malloc(size*sizeof(int*));
     if(board==NULL){
         printf("Error allocating memory for board\n");
@@ -63,8 +112,10 @@ int main(){
     }
     system("cls");
     print_grid(board,size);
-    free_grid( board, size);
+    add_number(board, size, score);
 
+
+    free_grid( board, size);
     return 0;
 
 
